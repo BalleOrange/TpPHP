10c98682e7e775176f44fe4313ad986a70c412b1
BalleOrange <balleorange2800@gmail.com>
2023-10-20 16:46:44 +0200

    Suite convergente

--- welcome.php
+++ welcome.php
@@ -1,7 +1,19 @@
 <html>
-<body>
+<body> 
  
-Quotient : <?php echo $_GET["txtDividende"] / $_GET["txtDiviseur"]; ?><br>
- 
+<?php
+$un = $_GET["txtDepart"];
+
+echo "Valeur pour le rang 0 : $un<br>";
+
+for ($i = 0; $i != $_GET["txtRang"] + 1; $i++)
+{
+    $calc = $un / 4 + 2;
+    $un = $calc;
+    echo "Valeur pour le $i", "eme rang : $un<br>"; 
+}
+
+?>
+
 </body>
 </html>
